a3cca05f623f108c6733a6ff257998c41fecb9c8
Dejan Radic <radic.dejan.nbg@gmail.com>
2017-12-18 15:50:45 +0100

    cao

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,26 @@
 $(function () {
 
 
+/************************************************************
 
+						  ZVEZDICE
+
+************************************************************/
+
+    $(".rateYo").rateYo({
+      rating: 3.6
+      });
+// Getter
+var starWidth = $(".rateYo").rateYo("option", "starWidth"); //returns 20px
+
+// Setter
+$(".rateYo").rateYo("option", "starWidth", "15px"); //returns a jQuery Element
+
+
+});
+$('.img-wrapper').mouseover(function(){
+    $('.restoran-hover').fadeIn(500);
+});
+$('.restoran-hover').mouseleave(function(){
+  $(this).fadeOut(500);
 });
