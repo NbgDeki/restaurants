4f4107bbfc4660145349c163050e33d5ee3b41b2
Dejan Radic <radic.dejan.nbg@gmail.com>
2017-12-20 14:18:57 +0100

    banana

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,30 @@
 $(function () {
 
-
+  $(".slider1").owlCarousel({
+    items: 4,
+    autoplay:true,
+    smartSpeed:700,
+    loop: true,
+    autoplayHoverPause: true,
+    dots: false,
+    responsive : {
+    // breakpoint from 0 up
+    0 : {
+    items: 1
+    },
+    // breakpoint from 480 up
+    480 : {
+    items: 2
+    },
+    // breakpoint from 768 up
+    768 : {
+    items: 4
+    }
+    }
+    });
 /************************************************************
 
-						  ZVEZDICE
+						              ZVEZDICE
 
 ************************************************************/
 
